c8fca2dfc8f240d7c03d2bccb20052996e0c60b1
Tim White <walton.timothy.james.white@gmail.com>
2024-03-25 14:49:02 +1300

    Debug logging

--- Browser Plugin/Display Headers Test/background.js
+++ Browser Plugin/Display Headers Test/background.js
@@ -3,6 +3,7 @@ let responseHeaders = [];
 function responseUrls(details) {
   if (details.type != null && details.method != null && details.requestId != null){
     responseHeaders.push(JSON.stringify(details, null, 2));
+    console.log("Saw HTTP request: " + JSON.stringify(details));		//DEBUG
   }
   chrome.storage.session.set({key: responseHeaders});
 }
