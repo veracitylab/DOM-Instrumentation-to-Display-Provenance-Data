b2a9658015a7dca2cbd5b1d51d81e488976a5973
Tim White <walton.timothy.james.white@gmail.com>
2024-03-26 15:21:27 +1300

    Display provenance data in a table

--- Browser Plugin/Display Headers Test/provenance-tab.js
+++ Browser Plugin/Display Headers Test/provenance-tab.js
@@ -6,16 +6,38 @@ const delimPos = provInfo.indexOf(";");
 const provId = provInfo.substring(0, delimPos);
 const sourceUrl = provInfo.substring(delimPos + 1);
 const provUrl = provenanceUrl(sourceUrl, provId);
-mainDiv.innerText = `The URL is ${document.URL} with prov ID ${provId} and source URL ${provUrl}.`;
+mainDiv.innerText = `Loading provenance data for source URL ${provUrl}, provenance ID ${provId}...`;
 
 function provenanceUrl(sourceUrl, provId) {
     return new URL(`/prov/${provId}`, sourceUrl).toString();
 }
 
-async function openProvenanceDataTab(provUrl) {
-    // e.preventDefault();
-    const provData = await (await fetch(provUrl)).json();
-    console.log("Provenance data for ", provUrl, " is:", provData);
+function wrap(elems, topLevel, perItem) {
+    return `<${topLevel}>` + elems.map((e) => `<${perItem}>${e}</${perItem}>`).join("") + `</${topLevel}>`;
 }
 
-openProvenanceDataTab(provUrl);
+function tableHeaderRow(elems) {
+    return wrap(elems, "tr", "th");
+}
+
+function tableRow(elems) {
+    return wrap(elems, "tr", "td");
+}
+
+function renderProvenanceHtml(sourceUrl, provId, provData) {
+    return `<h1>Provenance Information</h1><ul><li><b>Source URL:</b> ${sourceUrl}</li><li><b>Provenance ID:</b> ${provId}</li></ul>` +
+        "<table>" +
+        tableHeaderRow(["Activity Type", "End Time"]) +
+        provData.map((e) => tableRow([e.activities[0].type, e.activities[0].endTime])).join("") +
+        "</table>";
+}
+
+async function openProvenanceDataTab(sourceUrl, provId, provUrl) {
+    const provData = await (await fetch(provUrl)).json();
+    console.log("Provenance data for ", provUrl, " is:", provData);
+
+    mainDiv.innerHTML = renderProvenanceHtml(sourceUrl, provId, provData);
+    document.getElementsByTagName("title")[0].innerText = `Provenance Information for ${provUrl}`;
+}
+
+openProvenanceDataTab(sourceUrl, provId, provUrl);
