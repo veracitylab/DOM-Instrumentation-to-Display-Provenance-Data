3639e15b762dd35382a470287a6e14ec0633f239
Tim White <walton.timothy.james.white@gmail.com>
2024-03-27 21:04:10 +1300

    WIP converting to async to handle service worker termination

--- browser-extension/chrome/background.js
+++ browser-extension/chrome/background.js
@@ -1,8 +1,27 @@
-// Don't send messages to tabs before they are ready; queue them up instead
-let readyTabs = new Set();
-let unreadyQueue = new Map();
+console.log("Background process started!");
 
-function responseUrls(details) {
+// Don't send messages to tabs before they are ready; queue them up instead
+// let readyTabs = new Set();
+// let unreadyQueue = new Map();
+async function getReadyTabs() /*: Set<string> */ {
+  const fromStorage = await chrome.storage.session.get('readyTabs');
+  return new Set(fromStorage?.readyTabs ?? []);
+}
+
+async function setReadyTabs(readyTabs /*: Set<string> */) {
+  await chrome.storage.session.set({ readyTabs: [...readyTabs] }); // Store as array since chrome.storage can't handle Sets
+}
+
+async function getUnreadyQueue() /*: Map<string, string[]> */ {
+  const fromStorage = await chrome.storage.session.get('unreadyQueue');
+  return new Map(fromStorage?.unreadyQueue ? Object.entries(fromStorage?.unreadyQueue) : []);
+}
+
+async function setUnreadyQueue(unreadyQueue /*: Map<string, string[]> */) {
+  await chrome.storage.session.set({ unreadyQueue: Object.fromEntries(unreadyQueue) }); // Store as array of pairs since chrome.storage can't handle Maps
+}
+
+async function responseUrls(details) {
   if (details.type != null && details.method != null && details.requestId != null){
     console.log("Saw HTTP request: " + JSON.stringify(details));		//DEBUG
 
@@ -10,14 +29,18 @@ function responseUrls(details) {
     if (provIdInfo) {
       if (details.type === 'main_frame') {
         // A top-level frame load means we need to start queueing messages until the page indicates it's ready
+        const unreadyQueue = await getUnreadyQueue();
+        const readyTabs = await getReadyTabs();
         const existingQueue = unreadyQueue.get(details.tabId) ?? []
         console.log("Top-level frame load: Tab previously " + (readyTabs.has(details.tabId) ? "ready" : "not ready") + ". Will drop " + (unreadyQueue.get(details.tabId) ?? []).length + " queued messages.");
         unreadyQueue.delete(details.tabId);
+        await setUnreadyQueue(unreadyQueue);
         readyTabs.delete(details.tabId);
+        await setReadyTabs(readyTabs);
       }
   
       // sendOrQueue(details.tabId, { type: "responseHeader", details });
-      sendOrQueue(details.tabId, { type: "responseHeader", details: { url: details.url, provId: provIdInfo.value } });
+      await sendOrQueue(details.tabId, { type: "responseHeader", details: { url: details.url, provId: provIdInfo.value } });
     } else {
       console.log("Ignoring provenance-free message");
     }
@@ -30,34 +53,42 @@ chrome.webRequest.onHeadersReceived.addListener(
   ["responseHeaders"]
 );
 
-chrome.runtime.onMessage.addListener((msg, sender) => {
+chrome.runtime.onMessage.addListener(async (msg, sender) => {
   console.log("Received message: ", msg, " from ", sender);
   if (msg.type === 'ready') {
+    const readyTabs = await getReadyTabs();
     readyTabs.add(sender.tab.id);
-    flushUnreadyQueue(sender.tab.id);
+    await setReadyTabs(readyTabs);
+    await flushUnreadyQueue(sender.tab.id);
   }
 });
 
-function sendOrQueue(tabId, msg) {
+async function sendOrQueue(tabId, msg) {
+  const readyTabs = await getReadyTabs();
   if (readyTabs.has(tabId)) {
     console.log("Sending message immediately to ready tab " + tabId);
     send(tabId, msg);
   } else {
     console.log("Queueing message for non-ready tab " + tabId);
+    const unreadyQueue = await getUnreadyQueue();
     let messages = unreadyQueue.get(tabId) ?? [];
     messages.push(msg);
+    console.log("There are now " + messages.length + " messages in the queue.");
     unreadyQueue.set(tabId, messages);
+    await setUnreadyQueue(unreadyQueue);
   }
 }
 
-function flushUnreadyQueue(tabId) {
-  console.log("Flushing unready queue for newly ready tab " + tabId);
+async function flushUnreadyQueue(tabId) {
+  const unreadyQueue = await getUnreadyQueue();
+  console.log("Flushing " + unreadyQueue.get('' + tabId)?.length + " messages from unready queue for newly ready tab " + tabId);
   for (const msg of unreadyQueue.get(tabId) ?? []) {
     console.log("Sending message to newly ready tab " + tabId);
     send(tabId, msg);
   }
 
   unreadyQueue.delete(tabId);
+  await setUnreadyQueue(unreadyQueue);
 }
 
 function send(tabId, msg) {
