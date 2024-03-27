fb82730e248bb0d52c7d82557e284c593813d2cb
Tim White <walton.timothy.james.white@gmail.com>
2024-03-27 22:06:20 +1300

    WIP adding promise-chain sequencing to fix race

--- browser-extension/chrome/background.js
+++ browser-extension/chrome/background.js
@@ -1,24 +1,27 @@
 console.log("Background process started!");
 
+// Used to sequence loads and stores to avoid races
+let promiseChain = Promise.resolve();
+
 // Don't send messages to tabs before they are ready; queue them up instead
 // let readyTabs = new Set();
 // let unreadyQueue = new Map();
 async function getReadyTabs() /*: Set<string> */ {
-  const fromStorage = await chrome.storage.session.get('readyTabs');
+  const fromStorage = await appendToPromiseChain(() => chrome.storage.session.get('readyTabs'));
   return new Set(fromStorage?.readyTabs ?? []);
 }
 
 async function setReadyTabs(readyTabs /*: Set<string> */) {
-  await chrome.storage.session.set({ readyTabs: [...readyTabs] }); // Store as array since chrome.storage can't handle Sets
+  await appendToPromiseChain(() => chrome.storage.session.set({ readyTabs: [...readyTabs] })); // Store as array since chrome.storage can't handle Sets
 }
 
 async function getUnreadyQueue() /*: Map<number, string[]> */ {
-  const fromStorage = await chrome.storage.session.get('unreadyQueue');
+  const fromStorage = await appendToPromiseChain(() => chrome.storage.session.get('unreadyQueue'));
   return new Map(fromStorage?.unreadyQueue ?? []);
 }
 
 async function setUnreadyQueue(unreadyQueue /*: Map<number, string[]> */) {
-  await chrome.storage.session.set({ unreadyQueue: [...unreadyQueue.entries()] }); // Store as array of pairs since chrome.storage can't handle Maps
+  await appendToPromiseChain(() => chrome.storage.session.set({ unreadyQueue: [...unreadyQueue.entries()] })); // Store as array of pairs since chrome.storage can't handle Maps
 }
 
 async function responseUrls(details) {
@@ -29,18 +32,20 @@ async function responseUrls(details) {
     if (provIdInfo) {
       if (details.type === 'main_frame') {
         // A top-level frame load means we need to start queueing messages until the page indicates it's ready
-        const unreadyQueue = await getUnreadyQueue();
-        const readyTabs = await getReadyTabs();
-        const existingQueue = unreadyQueue.get(details.tabId) ?? []
-        console.log("Top-level frame load: Tab previously " + (readyTabs.has(details.tabId) ? "ready" : "not ready") + ". Will drop " + (unreadyQueue.get(details.tabId) ?? []).length + " queued messages.");
-        unreadyQueue.delete(details.tabId);
-        await setUnreadyQueue(unreadyQueue);
-        readyTabs.delete(details.tabId);
-        await setReadyTabs(readyTabs);
+        await appendToPromiseChain(async () => {
+          const unreadyQueue = await getUnreadyQueue();
+          const readyTabs = await getReadyTabs();
+          const existingQueue = unreadyQueue.get(details.tabId) ?? []
+          console.log("Top-level frame load: Tab previously " + (readyTabs.has(details.tabId) ? "ready" : "not ready") + ". Will drop " + (unreadyQueue.get(details.tabId) ?? []).length + " queued messages.");
+          unreadyQueue.delete(details.tabId);
+          await setUnreadyQueue(unreadyQueue);
+          readyTabs.delete(details.tabId);
+          await setReadyTabs(readyTabs);
+        });
       }
   
       // sendOrQueue(details.tabId, { type: "responseHeader", details });
-      await sendOrQueue(details.tabId, { type: "responseHeader", details: { url: details.url, provId: provIdInfo.value } });
+      await appendToPromiseChain(() => sendOrQueue(details.tabId, { type: "responseHeader", details: { url: details.url, provId: provIdInfo.value } }));
     } else {
       console.log("Ignoring provenance-free message");
     }
@@ -56,10 +61,12 @@ chrome.webRequest.onHeadersReceived.addListener(
 chrome.runtime.onMessage.addListener(async (msg, sender) => {
   console.log("Received message: ", msg, " from ", sender);
   if (msg.type === 'ready') {
-    const readyTabs = await getReadyTabs();
-    readyTabs.add(sender.tab.id);
-    await setReadyTabs(readyTabs);
-    await flushUnreadyQueue(sender.tab.id);
+    await appendToPromiseChain(async () => {
+      const readyTabs = await getReadyTabs();
+      readyTabs.add(sender.tab.id);
+      await setReadyTabs(readyTabs);
+      await flushUnreadyQueue(sender.tab.id);
+    });
   }
 });
 
@@ -93,4 +100,9 @@ async function flushUnreadyQueue(tabId) {
 
 function send(tabId, msg) {
   chrome.tabs.sendMessage(tabId, msg);
+}
+
+function appendToPromiseChain(func) {
+  promiseChain = promiseChain.then(func);
+  return promiseChain;
 }
