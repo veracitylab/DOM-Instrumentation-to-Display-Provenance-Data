07857785125c4b19c2a18012ae128fa71da17765
Tim White <walton.timothy.james.white@gmail.com>
2024-04-02 12:02:19 +1300

    Tidier

--- browser-extension/chrome/content.js
+++ browser-extension/chrome/content.js
@@ -1,59 +1,55 @@
 console.log("Content script running!");
 
-//TODO: fix indenting!
-function main() {
-
-console.log("Content script's main() is now running (hopefully after page loading completed)!");
-
-const provenanceDiv = document.createElement("div");
-provenanceDiv.style.position = 'fixed';
-provenanceDiv.style.top = '10px';
-provenanceDiv.style.left = '10px';
-provenanceDiv.style.zIndex = '1000'; // Hopefully on top of everything
-provenanceDiv.style.backgroundColor = 'lightgray';
-provenanceDiv.style.padding = '10px';
-provenanceDiv.style.fontFamily = 'sans-serif';
-provenanceDiv.innerHTML = '<span style="color: red; font-weight: bold"">▶ Prov</span>';
-const provButton = provenanceDiv.children[0];
-const responseList = document.createElement("ul");
-responseList.style.display = 'none';
-provenanceDiv.appendChild(responseList);
-document.getElementsByTagName("body")[0].appendChild(provenanceDiv);
-
-chrome.runtime.onMessage.addListener((msg) => {
-    console.log("Received message: ", msg);
-    if (msg.type === 'responseHeader') {
-        addEntry(msg.details.url, msg.details.provId);
-    }
-});
-
-function addEntry(sourceUrl, provId) {
-    const provenanceTabUrl = chrome.runtime.getURL("provenance-tab.html") + "#" + encodeURIComponent(provId + ";" + sourceUrl);
-    const item = document.createElement("li");
-    item.innerHTML = `<a href="${provenanceTabUrl}" target="_blank"><b>${provId}:</b> ${sourceUrl}</a>`;
-    responseList.appendChild(item);
-}
-
-function toggleProvDisplay() {
-    if (responseList.style.display === 'none') {
-        provButton.innerText = '▼ Prov';
-        responseList.style.display = 'inline';
-    } else {
-        provButton.innerText = '▶ Prov';
-        responseList.style.display = 'none';
-    }
-}
-
-provButton.onclick = toggleProvDisplay;
-
-// Let the extension know that we are ready to handle messages.
-// This is necessary to avoid a race with the main page load.
-console.log("Content script about to send ready message...");
-chrome.runtime.sendMessage({ type: "ready" });
-console.log("Content script has sent the ready message!");
-
-
-}
-
 // Since we are now being injected ASAP, need to wait until the document finishes loading before we actually run the main code.
+function main() {
+    console.log("Content script's main() is now running (hopefully after page loading completed)!");
+
+    const provenanceDiv = document.createElement("div");
+    provenanceDiv.style.position = 'fixed';
+    provenanceDiv.style.top = '10px';
+    provenanceDiv.style.left = '10px';
+    provenanceDiv.style.zIndex = '1000'; // Hopefully on top of everything
+    provenanceDiv.style.backgroundColor = 'lightgray';
+    provenanceDiv.style.padding = '10px';
+    provenanceDiv.style.fontFamily = 'sans-serif';
+    provenanceDiv.innerHTML = '<span style="color: red; font-weight: bold"">▶ Prov</span>';
+    const provButton = provenanceDiv.children[0];
+    const responseList = document.createElement("ul");
+    responseList.style.display = 'none';
+    provenanceDiv.appendChild(responseList);
+    document.getElementsByTagName("body")[0].appendChild(provenanceDiv);
+
+    chrome.runtime.onMessage.addListener((msg) => {
+        console.log("Received message: ", msg);
+        if (msg.type === 'responseHeader') {
+            addEntry(msg.details.url, msg.details.provId);
+        }
+    });
+
+    function addEntry(sourceUrl, provId) {
+        const provenanceTabUrl = chrome.runtime.getURL("provenance-tab.html") + "#" + encodeURIComponent(provId + ";" + sourceUrl);
+        const item = document.createElement("li");
+        item.innerHTML = `<a href="${provenanceTabUrl}" target="_blank"><b>${provId}:</b> ${sourceUrl}</a>`;
+        responseList.appendChild(item);
+    }
+
+    function toggleProvDisplay() {
+        if (responseList.style.display === 'none') {
+            provButton.innerText = '▼ Prov';
+            responseList.style.display = 'inline';
+        } else {
+            provButton.innerText = '▶ Prov';
+            responseList.style.display = 'none';
+        }
+    }
+
+    provButton.onclick = toggleProvDisplay;
+
+    // Let the extension know that we are ready to handle messages.
+    // This is necessary to avoid a race with the main page load.
+    console.log("Content script about to send ready message...");
+    chrome.runtime.sendMessage({ type: "ready" });
+    console.log("Content script has sent the ready message!");
+}
+
 window.addEventListener('DOMContentLoaded', main);
