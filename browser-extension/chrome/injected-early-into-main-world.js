dacfe25f4c128dd4d59cb1a6eac663acbdb1601e
Tim White <walton.timothy.james.white@gmail.com>
2024-04-08 10:45:09 +1200

    WIP trying to factor code common to MAIN and ISOLATED worlds into a separate file, but Chromium will only load it for one

--- browser-extension/chrome/injected-early-into-main-world.js
+++ browser-extension/chrome/injected-early-into-main-world.js
@@ -217,42 +217,6 @@ function mutationObserverCallback(mutationList, observer) {
     }
 };
 
-function makeHighlightRectDivFor(elem, cls) {
-    const rect = elem.getBoundingClientRect();
-    if (rect.width === 0 || rect.height === 0) {
-        console.log(`Ignoring zero-area rect for element `, elem);
-        return undefined;
-    }
-
-    console.log(`Adding highlight rect at `, rect, ` with class ${cls}.`);
-    const div = document.createElement('div');
-    div.classList.add(cls);
-    div.style.position = 'absolute';
-    for (const prop of ['left', 'top', 'width', 'height']) {
-        div.style[prop] = rect[prop] + "px";
-    }
-    div.style.backgroundColor = 'yellow';
-    div.style.opacity = '30%';
-    div.style.zIndex = 9999;
-    div.style.pointerEvents = 'none';
-    return div;
-}
-
-function addHighlightRectFor(elem, cls) {
-    const div = makeHighlightRectDivFor(elem, cls);
-    if (div) {
-        document.body.appendChild(div);
-    }
-}
-
-function removeHighlightRects(cls) {
-    console.log(`Removing all highlight rects with class ${cls}.`);
-    for (const elem of Array.from(document.getElementsByClassName(cls))) {
-        elem.remove();
-    }
-}
-
-
 // Try running it a bit later
 function later() {
 
